d719a804afb208ef7cab405832679ca78ed38f38
RooieDirk <dirksmits@zonnet.nl>
2016-06-28 21:21:36 +0200

    add shared-vars file

---
+++ rtl-ais/include/shared_vars.h
@@ -0,0 +1,10 @@
+
+#ifndef INC_SHARED_VARS_H
+#define INC_SHARED_VARS_H
+
+
+#include "rtl-sdr.h"
+extern  rtlsdr_dev_t *dev ;
+extern  int ppm_error ;
+extern int use_autotune;
+#endif //INC_SHARED_VARS_H
